483ae4c42ab86d93059f89f3ccb533f817cbb55c
David Poole <dpoole@cradlepoint.com>
2019-06-04 13:07:46 -0600

    better wiface script

---
+++ wiface.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# list wireless interfaces
+#
+# designed to be used from shell, e.g. :
+# 	wpa_sli -i $(wiface 1) status
+
+set -eu
+
+counter=0
+for f in /sys/class/net/wlp* ; 
+do
+	. $f/uevent
+	if [ $# -eq 0 ] ; 
+	then
+		echo $counter $INTERFACE 
+	else
+		if [ $counter -eq $1 ] ; 
+		then
+			echo $INTERFACE
+			break
+		fi
+	fi
+
+	counter=$((counter+1))
+done
+
